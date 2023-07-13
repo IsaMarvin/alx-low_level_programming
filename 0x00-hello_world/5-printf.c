a573b7287dd68c1fa3125b5e447167644de76da4
IsaMarvin <marvinisra08@gmail.com>
2023-07-13 04:27:50 -0700

    Add program to print a specific string using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - displays "with proper grammar" with new line
+ * Return: 0 and exit the program
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
