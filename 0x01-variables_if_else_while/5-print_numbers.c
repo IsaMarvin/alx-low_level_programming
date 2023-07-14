80644d4d75491f8e4f0967eee678963fc7fecfca
IsaMarvin <marvinisra08@gmail.com>
2023-07-14 06:07:57 -0700

    [Feature] Add code to print single digit numbers in base 10 using putchar

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - display numbers from 0 to 9
+ * Return: return 0 and exit the program.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
