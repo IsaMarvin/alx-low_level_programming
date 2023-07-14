784f310177671763abf5fd410c563004b612825f
IsaMarvin <marvinisra08@gmail.com>
2023-07-14 05:46:26 -0700

    [Enhancement] Add code to determine the last digit of a random number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - display the last digit of the number stored in the variable n
+ * Return: return 0 and exit the program.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is %d ", n, n % 10);
+	if (n % 10 > 5)
+		printf("and is greater than 5\n");
+	else if (n % 10 == 0)
+		printf("and is 0\n");
+	else if (n % 10 < 6 && n % 10 != 0)
+		printf("and is less than 6 and not 0\n");
+	return (0);
+}
