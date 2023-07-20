dcb85cb970fe8272d7935a00f06ca1619261429a
IsaMarvin <marvinisra08@gmail.com>
2023-07-20 14:52:58 -0700

    [Function] Add print_line function in 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - check the code.
+ *
+ *@n: variable
+ *
+ * Return: Always
+ */
+
+void print_line(int n)
+{
+	int c;
+
+	for (c = 0; c < n; c++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,6 +8,7 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
 
 #endif /* MAIN_H */
 
