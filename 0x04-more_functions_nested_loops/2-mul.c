c0b18680f2c09fa27b5b19177c76184132f94f9f
IsaMarvin <marvinisra08@gmail.com>
2023-07-20 14:18:50 -0700

    [Function] Add mul function in 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: The product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,6 +3,7 @@
 
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 
 #endif /* MAIN_H */
 
