8839c7dbef20057a1717df29b5c20df9e1ad0738
IsaMarvin <marvinisra08@gmail.com>
2023-07-24 14:07:03 -0700

    feat: Implement reset_to_98 function
    Create a new function named reset_to_98 that takes a pointer to an integer as a parameter. The purpose of this function is to update the value of the integer it points to, setting it to 98. This is achieved by dereferencing the pointer and assigning the value 98 to the memory location it points to.
    
    Function signature:
    void reset_to_98(int *n);
    
    This commit introduces the new reset_to_98 function and includes a test in main to verify its functionality.
    
    Task reference: #0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - Takes a pointer to an int as a parameter.
+ * @n: type int pointer
+ * description: Update the value to 98
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
