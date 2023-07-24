186fa76488b19ce9658393e4f02416e636eb49a6
IsaMarvin <marvinisra08@gmail.com>
2023-07-24 14:31:59 -0700

    feat: Implement _strlen function
    Create a function _strlen that calculates and returns the length of the input string s. The function iterates through the characters of the string until it encounters the null terminator '\0', which marks the end of the string. The number of characters encountered during the iteration corresponds to the length of the string.
    
    Function signature:
    int _strlen(char *s);
    
    Task: 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - Returns the lenght of a string.
+ * @s: Type char pointer
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+	{
+	}
+		return (c);
+
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,5 +3,6 @@
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 
 #endif
