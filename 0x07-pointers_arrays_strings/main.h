fab4bad0f41fd746eae0b5d5286b78f510f154c7
IsaMarvin <marvinisra08@gmail.com>
2023-07-31 13:13:15 -0700

    [Task 4] Implement the _strpbrk() function in the 4-strpbrk.c file.
    The _strpbrk() function searches the string s for any of the bytes in the string accept. It takes a pointer to the string s and a pointer to the string accept as arguments. The function returns a pointer to the first byte in s that matches one of the bytes in accept or NULL if no such byte is found.
    
    This function provides a custom implementation of the strpbrk() function, commonly found in the standard library, to search a string for any of a set of bytes.
    
    Prototype: char *_strpbrk(char *s, char *accept);

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 
 #endif
