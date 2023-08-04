2c4b46d077ecbf5fa93517e5fdbc00b79330d5ef
IsaMarvin <marvinisra08@gmail.com>
2023-08-04 13:37:38 -0700

    [Task 0] Implement program to print its name
    Implement a program in the 0-whatsmyname.c file that prints its name, followed by a new line. The program utilizes the argv array to access its own name and prints it to the standard output.
    
    The program is compiled using `gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-whatsmyname.c -o mynameis` command.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints name folowed by a new line.
+ * @argc: type int argument
+ * @argv: type char argument of string.
+ * Return: none
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
