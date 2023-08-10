93ec1e8f755cecf531930fb7976433fddefb6ca8
IsaMarvin <marvinisra08@gmail.com>
2023-08-10 00:28:53 -0700

    Task 101: Implement a Program to Multiply Two +num
    Develop a program named '101-mul.c' that takes two positive numbers as arguments and multiplies them. The program verifies the arguments and performs the multiplication. It prints the result followed by a newline. If the number of arguments is incorrect or if the arguments are not valid digits, the program prints an error message and exits with a status of 98.
    
    Filename: 101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all arguments received.
+ * @argc: type int argument
+ * @argv: type char argument of string.
+ * Return: if not receive 2 arg, rt error
+ */
+int main(int argc, char *argv[])
+{
+	int n1, n2, mul;
+
+	if (argc != 3)
+		printf("Error\n");
+	else
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		mul = n1 * n2;
+		printf("%d\n", mul);
+	}
+	return (0);
+}
