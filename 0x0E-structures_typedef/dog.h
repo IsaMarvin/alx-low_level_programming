5066b6f923e3b456bd87bd78fb430052f31b2d41
IsaMarvin <marvinisra08@gmail.com>
2023-08-15 12:32:34 -0700

    [0x0E_3]Define 'dog_t' Type Alias for 'struct dog'
    Introduce a new type alias 'dog_t' for the 'struct dog' type in the 'dog.h' file. This type alias provides a more convenient way to refer to the 'struct dog' type.
    
    The '3-main.c' file demonstrates the usage of the 'dog_t' type alias by initializing a 'dog_t' variable named 'my_dog' with specific values and then printing the dog's name and age.
    
    Task Number: #3
    File Names: dog.h, 3-main.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - Django created you a struct
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- *
- * Description: Data structure for holbertn and Django
+ * struct dog - The Dog main struct.
+ * @name: The dog name goes here.
+ * @age: The age of that dog.
+ * @owner: Who the owner? of the dog.
  */
 typedef struct dog
 {
