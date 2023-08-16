72b3a5aee116c60482efa426dbacc5df6b083e78
IsaMarvin <marvinisra08@gmail.com>
2023-08-16 15:15:16 -0700

    [0x0F_3] Implement Function Selector for Calc
    Implement the 'get_op_func' function in the '3-get_op_func.c' file. This function takes a string argument representing an operator and returns a function pointer to the corresponding operator function ('op_add', 'op_sub', 'op_mul', 'op_div', or 'op_mod'). If the provided operator does not match any of the expected operators, the function returns NULL.
    
    Task Number: #3
    File Name: 3-get_op_func.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,50 @@
+/**
+ * op_add - sum of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substract of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - module of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
