c398d8c53ca1779a472cd9e99929716c04d9f151
IsraMarvin <marvinisra08@gmail.com>
2023-10-19 04:35:41 +0500

    [0x12-5] Implement the print_before_main function
    In this commit, I've implemented the `print_before_main` function. This function is designed to print the specified two lines of text to the standard output before the `main` function is executed.
    
    Compile and test command:
    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 100-main.c 100-first.c -o first
    
    Function Prototype:
    void before_main(void)__attribute__((constructor));
    
    File: 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * before_main - Function executed before main.
+ *
+ * Description: This function is executed before the main function and
+ * prints two lines of text.
+ *
+ * Return: Void (no return value)
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -26,5 +26,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void before_main(void)__attribute__((constructor));
 
 #endif
